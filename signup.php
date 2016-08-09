83405706f3e7b53849943dc31fc4d686377eb3fb
davidferguson <davidferguson@users.noreply.github.com>
2016-08-09 14:20:18 +0200

    made signup page work

--- signup.php
+++ signup.php
@@ -1,10 +1,35 @@
 <?php
 
-$email = $_POST["email"];
-$username = $_POST["username"];
-$password = $_POST["password"];
-$confirm_password = $_POST["cpassword"];
+$email = mysql_real_escape($_POST["email"]);
+$username = mysql_real_escape($_POST["username"]);
+$password = mysql_real_escape($_POST["password"]);
+$confirm_password = mysql_real_escape($_POST["cpassword"]);
 
-echo $email;
+if ( $password != $confirm_password ) {
+echo "Passwords did not match";
+break;
+}
+
+$servername = "127.0.0.1";
+$dbusername = "notifree";
+$dbpassword = "prewired2016";
+$dbname = "notifree";
+
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO users VALUES ('0', '" . $username . "', '" . $password . "', '" . $email . "')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
 
 ?>
