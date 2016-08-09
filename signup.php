a91a5c05872c7f61cb0b79ad7be81f52127fc4f0
davidferguson <davidferguson@users.noreply.github.com>
2016-08-09 15:10:00 +0200

    added password verification

--- signup.php
+++ signup.php
@@ -1,15 +1,5 @@
 <?php
 
-$email = mysql_real_escape($_POST["email"]);
-$username = mysql_real_escape($_POST["username"]);
-$password = mysql_real_escape($_POST["password"]);
-$confirm_password = mysql_real_escape($_POST["cpassword"]);
-
-if ( $password != $confirm_password ) {
-echo "Passwords did not match";
-break;
-}
-
 $servername = "127.0.0.1";
 $dbusername = "notifree";
 $dbpassword = "prewired2016";
@@ -22,10 +12,32 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$email = mysqli_real_escape_string($conn, $_POST["email"]);
+$username = mysqli_real_escape_string($conn, $_POST["username"]);
+$password = mysqli_real_escape_string($conn, $_POST["password"]);
+$confirm_password = mysqli_real_escape_string($conn, $_POST["cpassword"]);
+
+if ( $password != $confirm_password ) {
+echo "Passwords did not match";
+break;
+}
+
+
+echo strlen($password);
+if ( strlen($password) < 6 ) {
+echo "Password was not of minimum length";
+break;
+}
+
+if(1 != preg_match('~[0-9]~', $password)) {
+echo "Password must contain numbers";
+break;
+}
+
 $sql = "INSERT INTO users VALUES ('0', '" . $username . "', '" . $password . "', '" . $email . "')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    header("Location: /");
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
