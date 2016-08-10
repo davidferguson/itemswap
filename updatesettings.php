e631a97a71584f4ab2ab66d73f084f0fb52ae505
David Ferguson <davidferguson@users.noreply.github.com>
2016-08-10 20:11:48 +0100

    formatted updatesettings.php

--- updatesettings.php
+++ updatesettings.php
@@ -1,29 +1,36 @@
 <?php
-session_start();
-$servername = "127.0.0.1";
-$dbusername = "notifree";
-$dbpassword = "prewired2016";
-$dbname = "notifree";
 
-// Create connection
-$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // start the session - required to get username variable
+    session_start();
 
-$email = mysqli_real_escape_string($conn, $_POST["email"]);
+    // define mysql server details
+    $servername = "127.0.0.1";
+    $dbusername = "notifree";
+    $dbpassword = "prewired2016";
+    $dbname = "notifree";
 
+    // connect to server, displaying message on error
+    $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-$sql = "UPDATE `users` SET email = '". $email . "' WHERE username = '" . $_SESSION["username"] ."'";
+    // get the POST data, and escape it
+    $email = mysqli_real_escape_string($conn, $_POST["email"]);
 
+    // generate the UPDATE mysql query
+    $sql = "UPDATE `users` SET email = '". $email . "' WHERE username = '" . $_SESSION["username"] ."'";
 
-if ($conn->query($sql) === TRUE) {
-    header("Location: settings.php");
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+    if ($conn->query($sql) === TRUE) {
+        // if the query executed successfully, take the user back to the settings page
+        header("Location: settings.php");
+    } else {
+        // if there was an error, tell the user
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 
-$conn->close();
+    // close the database connection at the end
+    $conn->close();
 
 ?>
