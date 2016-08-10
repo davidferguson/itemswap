bd7f270192dc30d39971a8bb3db2d9b8c849e50e
David Ferguson <davidferguson@users.noreply.github.com>
2016-08-10 19:29:50 +0100

    formatted Login.php

--- Login.php
+++ Login.php
@@ -1,37 +1,39 @@
 <?php
 
-$servername = "127.0.0.1";
-$dbusername = "notifree";
-$dbpassword = "prewired2016";
-$dbname = "notifree";
+    // define mysql server details
+    $servername = "127.0.0.1";
+    $dbusername = "notifree";
+    $dbpassword = "prewired2016";
+    $dbname     = "notifree";
 
-// Create connection
-$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // connect to server, displaying message on error
+    $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-$username = mysqli_real_escape_string($conn, $_POST["username"]);
-$password = mysqli_real_escape_string($conn, $_POST["password"]);
+    // get the POST data, and escape it
+    $username = mysqli_real_escape_string($conn, $_POST["username"]);
+    $password = mysqli_real_escape_string($conn, $_POST["password"]);
 
+    // generate the SELECT mysql query and run it
+    $sql    = "SELECT * FROM `users` where username = '" . $username . "' and password = '" . $password . "'";
+    $result = mysqli_query($conn, $sql);
 
-$sql = "SELECT * FROM `users` where username = '".$username."' and password = '".$password."'";
-$result = mysqli_query($conn, $sql);
-if( $result ) {
-	$rowcount = mysqli_num_rows( $result );
-	if ( $rowcount != 0 ) {
-		echo "logged in";
-		session_start();
-		$_SESSION["username"]= $username;
-		header("Location: /");
-	} else {
-		echo "not logged in";
-	}
-} else {
-	echo "error";
-}
-$conn->close();
-
+    // check if the query executed successfully
+    if ($result) {
+        $rowcount = mysqli_num_rows($result);
+        if ($rowcount != 0) {
+            // if the query executed successfully and produced a result, log the user in
+            session_start();
+            $_SESSION["username"] = $username;
+            header("Location: /");
+        } else {
+            echo "not logged in";
+        }
+    } else {
+        echo "MySQL Query Error";
+    }
+    $conn->close();
 
 ?>
