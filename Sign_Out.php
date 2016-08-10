87d7c5165671428cdc9cfd05169e8019d2d62466
David Ferguson <davidferguson@users.noreply.github.com>
2016-08-10 19:35:41 +0100

    formatted Sign_Out.php

--- Sign_Out.php
+++ Sign_Out.php
@@ -1,8 +1,8 @@
 <?php
 
-session_start();
-session_destroy();
-
-header("Location: /");
+  // destroy the session, and take the user back to the home page
+  session_start();
+  session_destroy();
+  header("Location: /");
 
 ?>
