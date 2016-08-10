ec6eda15511c18d87cf01d10ac910ad572f4d73a
Lzom142 <Lewismclaren100@gmail.com>
2016-08-10 10:44:09 +0100

    new login
    updated login

--- Login.php
+++ Login.php
@@ -17,14 +17,18 @@ $password = mysqli_real_escape_string($conn, $_POST["password"]);
 
 
 $sql = "SELECT * FROM `users` where username = '".$username."' and password = '".$password."'";
-/*
-if ($conn->query($sql) === TRUE) {
-    header("Location: /");
+$result = mysqli_query($conn, $sql);
+if( $result ) {
+	$rowcount = mysqli_num_rows( $result );
+	if ( $rowcount != 0 ) {
+		echo "logged in";
+	} else {
+		echo "not logged in";
+	}
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+	echo "error";
 }
-*/
-echo $sql;
 $conn->close();
+
 
 ?>
